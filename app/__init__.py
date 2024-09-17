a02431b32d38c5ddc48a4b6b783326f095ed8954
Tom Plant <tom@tplant.com.au>
2024-09-18 01:21:03 +1000

    use POSTGRES_URL

--- app/__init__.py
+++ app/__init__.py
@@ -75,6 +75,7 @@ def create_app():
     app = Flask(__name__)
     
     app.config.from_object(settings)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['POSTGRES_URL']
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(500, internal_server_error)
 
